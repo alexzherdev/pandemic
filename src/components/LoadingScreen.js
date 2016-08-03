d1b27b0409c407502e5645705481e978bd35c33c
Alex Zherdev <alex.zherdev@gmail.com>
2016-08-03 23:29:02 +0300

    Preload loading indicator

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Preload from 'react-preload';
 
 
 const LoadingScreen = () =>
   <div className="loading-screen">
-    <h1 className="text-danger">Loading...</h1>
-    <img src={require('../assets/images/infection_rate.png')} />
+    <Preload
+        loadingIndicator={<div />}
+        images={[require('../assets/images/infection_rate.png')]}
+        autoResolveDelay={10000}
+        resolveOnError={true}
+        mountChildren={true}>
+      <h1 className="text-danger">Loading...</h1>
+      <img src={require('../assets/images/infection_rate.png')} />
+    </Preload>
   </div>;
 
 export default LoadingScreen;
