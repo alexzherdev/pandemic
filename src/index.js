7eb1ac598917760dd93c31da272d77f750c797da
Alex Zherdev <alex.zherdev@gmail.com>
2016-08-05 08:46:37 +0300

    Introduce google analytics

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
+import ReactGA from 'react-ga';
 
 import routes from './routes';
 import configureStore from './store/configureStore';
@@ -18,11 +19,22 @@ import './assets/styles/styles.scss';
 import 'jquery';
 import 'web-animations-js/web-animations.min';
 
+
 const store = configureStore();
 const history = syncHistoryWithStore(browserHistory, store);
 
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+}
+
+ReactGA.initialize('UA-82047208-1');
+
 render(
   <Provider store={store}>
-    <Router history={history} routes={routes} />
+    <Router
+      history={history}
+      routes={routes}
+      onUpdate={logPageView} />
   </Provider>, document.getElementById('app')
 );
